1ce0e7068785238ec67dc0092f0d1614f0e46392
Ney <ney@pop-os.localdomain>
2019-09-16 15:55:23 -0400

    La ventana tiene labels y cajas de texto falta insertar valor por caja de texto

--- Logic/Polinomio.java
+++ Logic/Polinomio.java
@@ -21,53 +21,60 @@ public class Polinomio{
     V_Coef = new double[dimFinal+5];
     V_Exp = new int [dimFinal+5];
   }
+  
+  public boolean Pertenece(char caracter){
+    String auxCadena = Character.toString(caracter);
+    return auxCadena.matches("[-+]");
+  }
   ///
   
   //Insertar monomio
   public void Insertar(char signo, double coeficiente, int exponente){
-    
-    if(coeficiente == 0){
-        System.out.println("Error: coeficiente es igual a cero");
-    }else{
-      if(signo == '-'){
-        coeficiente = - coeficiente;
-        if(dim == -1){
-          dim++;
-          V_Coef[dim] = coeficiente;
-          V_Exp[dim] = exponente;
-          System.out.println(V_Coef[dim]);
-          System.out.println(exponente);
-        }
+  
+    if(Pertenece(signo)){
+      if(coeficiente == 0){
+          System.out.println("Error: coeficiente es igual a cero");
       }else{
-        int i=0;
-        while ((i <= dim)&&(V_Exp[i] > exponente)){
-          i++;
+        if(signo == '-'){
+          coeficiente = - coeficiente;
         }
-        if(i <= dim){
-          if( exponente != V_Exp[i]){
-            //redimencionar();
-            for(int aux=dim; aux > 1; aux--){
-              V_Coef[aux+1] = V_Coef[aux];
-              V_Exp[aux+1] = V_Exp[aux];
-            }
-            V_Coef[i] = coeficiente;
-            V_Exp[i] = exponente;
+        if(dim == -1){
             dim++;
-          }else{
-            double Suma = coeficiente + V_Coef[i];
-            if (Suma == 0){
-              for (int aux2 = i; aux2 < dim; aux2++){
-                V_Coef[aux2] = V_Coef[aux2 + 1];
-                V_Exp[aux2] = V_Exp[aux2+1];
+            V_Coef[dim] = coeficiente;
+            V_Exp[dim] = exponente;
+            System.out.println(V_Coef[dim]);
+            System.out.println(exponente);
+        }else{
+          int i=0;
+          while ((i <= dim)&&(V_Exp[i] > exponente)){
+            i++;
+          }
+          if(i <= dim){
+            if( exponente != V_Exp[i]){
+              //redimencionar();
+              for(int aux=dim; aux > 1; aux--){
+                V_Coef[aux+1] = V_Coef[aux];
+                V_Exp[aux+1] = V_Exp[aux];
               }
-              dim--;
+              V_Coef[i] = coeficiente;
+              V_Exp[i] = exponente;
+              dim++;
             }else{
-              V_Coef[i] = Suma;
+              double Suma = coeficiente + V_Coef[i];
+              if (Suma == 0){
+                for (int aux2 = i; aux2 < dim; aux2++){
+                  V_Coef[aux2] = V_Coef[aux2 + 1];
+                  V_Exp[aux2] = V_Exp[aux2+1];
+                }
+                dim--;
+              }else{
+                V_Coef[i] = Suma;
+              }
             }
           }
         }
       }
-    }
+  }
   
   }
   
--- ventanaPoli.java
+++ ventanaPoli.java
@@ -9,15 +9,22 @@ import javax.swing.*;
 import java.util.Scanner;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.border.Border;
 import javax.swing.JOptionPane;
 
 public class ventanaPoli{
   //Atributos
   private Polinomio poli = new Polinomio();
   private JFrame ventana;
+  private JTextField SignoTxt;
   private JTextField CoefTxt;
   private JTextField ExpTxt;
   private JButton SumarBoton;
+  private JLabel labelSigno;
+  private JLabel labelCoef;
+  private JLabel labelExp;
   
   //Clase pincipal
   public static void main(String[] args){  
@@ -35,12 +42,32 @@ public class ventanaPoli{
     ventana.setLayout(new GridBagLayout());  
     
     //Declarando caja de Coeficiente
+    SignoTxt = new JTextField("        ");
+    Border bordeSigno = BorderFactory.createLineBorder(Color.pink, 2);
+    SignoTxt.setBorder(bordeSigno);
+    
     CoefTxt = new JTextField("        ");
+    Border bordeCoef = BorderFactory.createLineBorder(Color.pink, 2);
+    CoefTxt.setBorder(bordeCoef);
+    
     ExpTxt = new JTextField("       ");
+    Border bordeExp = BorderFactory.createLineBorder(Color.pink, 2);
+    ExpTxt.setBorder(bordeExp);
     
     //Declaramos botón Sumar
     SumarBoton = new JButton("Suma");
     SumarBoton.setBackground(Color.pink);
+    
+    //Declaramos los labels
+    
+    labelSigno= new JLabel("Signo");
+    labelSigno.setFont(new Font("Arial", 0, 18));
+    
+    labelCoef= new JLabel("Coeficiente");
+    labelCoef.setFont(new Font("Arial", 0, 18));
+    
+    labelExp = new JLabel("Exponente");
+    labelExp.setFont(new Font("Arial", 0, 18));
     
     //Objeto de configuración del grid
     GridBagConstraints gridConf = new GridBagConstraints();
@@ -50,20 +77,31 @@ public class ventanaPoli{
     /* Adherir elementos a la ventana*/
     /*-------------------------------*/
     
-    //Confi de pocicion CoefTxt
+    //Confi de pocicion SignoTxt
     gridConf.gridx = 2;
     gridConf.gridy = 0;
     gridConf.ipady = 20;
     gridConf.ipadx = 15;
     gridConf.insets = new Insets(0,0,3,3); //padding
     gridConf.gridwidth = 2;
+    
+    //Añadir SignoTxt a la ventana
+    ventana.add(SignoTxt, gridConf);
+    
+    //Confi de pocicion CoefTxt
+    gridConf.gridx = 2;
+    gridConf.gridy = 1;
+    gridConf.insets = new Insets(0,0,3,3); //padding
+    gridConf.gridwidth = 2;
+    
     //Añadir CoefTxt a la ventana
     ventana.add(CoefTxt, gridConf);
     
     //Confi de pocicion ExpTxt
     gridConf.gridx = 2;
-    gridConf.gridy = 1;
+    gridConf.gridy = 2;
     gridConf.insets = new Insets(0,0,0,0);
+    
     //Añadir ExpTxt a la ventana
     ventana.add(ExpTxt, gridConf);
     
@@ -75,8 +113,23 @@ public class ventanaPoli{
       
     //Ayadir SumaBoton a la ventana
     gridConf.gridx = 0;
-    gridConf.gridy = 2;
+    gridConf.gridy = 3;
     ventana.add(SumarBoton, gridConf);
+      
+    //Añadir labels a la ventana
+      gridConf.gridx = 0;
+      gridConf.gridy = 0;
+      gridConf.ipady = 20;
+      gridConf.ipadx = 15;
+      //gridConf.insets = new Insets(0,0,0,0); //padding
+      //gridConf.gridwidth = 2;
+      ventana.add(labelSigno, gridConf); 
+      
+      gridConf.gridy = 1;
+      ventana.add(labelCoef, gridConf);
+      
+      gridConf.gridy = 2;
+      ventana.add(labelExp, gridConf);
       
     //onclick SumaBoton
     SumarBoton.addActionListener(new ActionListener(){
